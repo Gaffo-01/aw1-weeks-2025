cfb471902d73558e3ab7642daaaa722d26fa365a
MATTEO GAFFOGLIO s285259 <s285259@studenti.polito.it>
2025-04-08 19:48:15 +0200

    commenti form and stato

--- week07/react-qa/src/App.jsx
+++ week07/react-qa/src/App.jsx
@@ -7,7 +7,13 @@ import NavHeader from "./components/NavHeader";
 import QuestionDescription from "./components/QuestionDescription";
 import Answers from "./components/Answers";
 
-const fakeQuestion = new Question(1, "Is JavaScript better than Python?", "luigi.derussis@polito.it", 1, "2025-02-28");
+const fakeQuestion = new Question(
+  1,
+  "Is JavaScript better than Python?",
+  "luigi.derussis@polito.it",
+  1,
+  "2025-02-28"
+);
 fakeQuestion.init();
 const fakeAnswers = fakeQuestion.getAnswers();
 
@@ -16,46 +22,75 @@ function App() {
   const [answers, setAnswers] = useState(fakeAnswers);
 
   const voteUp = (answerId) => {
-    setAnswers(oldAnswers => {
-      return oldAnswers.map(ans => {
-        if(ans.id === answerId)
-          return new Answer(ans.id, ans.text, ans.email, ans.userId, ans.date, ans.score +1);
-        else
-          return ans;
+    setAnswers((oldAnswers) => {
+      return oldAnswers.map((ans) => {
+        if (ans.id === answerId)
+          return new Answer(
+            ans.id,
+            ans.text,
+            ans.email,
+            ans.userId,
+            ans.date,
+            ans.score + 1
+          );
+        else return ans;
       });
     });
-  }
+  };
 
   const addAnswer = (answer) => {
-    setAnswers(oldAnswers => {
+    setAnswers((oldAnswers) => {
       // temporaneo
-      const newId = Math.max(... oldAnswers.map(ans => ans.id)) + 1;
-      const newAnswer = new Answer(newId, answer.text, answer.email, undefined, answer.date);
+      const newId = Math.max(...oldAnswers.map((ans) => ans.id)) + 1;
+      const newAnswer = new Answer(
+        newId,
+        answer.text,
+        answer.email,
+        undefined,
+        answer.date
+      );
       return [...oldAnswers, newAnswer];
     });
-  }
+  };
 
+  /*modifica ma non sappiamo su quale campo 
+  => riportiamo tutta la risposta non solo un campo */
   const updateAnswer = (answer) => {
-    setAnswers(oldAnswers => {
-      return oldAnswers.map(ans => {
-        if(ans.id === answer.id)
-          return new Answer(answer.id, answer.text, answer.email, ans.userId, answer.date, ans.score);
-        else
-          return ans;
+    setAnswers((oldAnswers) => {
+      return oldAnswers.map((ans) => {
+        if (ans.id === answer.id)
+          return new Answer(
+            /*answer lo prendiamo dal form */
+            answer.id,
+            answer.text,
+            answer.email,
+            ans.userId,
+            answer.date,
+            ans.score
+          );
+        else return ans;
       });
     });
-  }
+  };
 
   return (
     <>
       <NavHeader questionNum={question.id} />
       <Container fluid className="mt-3">
         <QuestionDescription question={question} />
-        <Answers answers={answers} voteUp={voteUp} addAnswer={addAnswer} editAnswer={updateAnswer}/>
+        <Answers
+          /*per le props conviene chiamarle uguali 
+          per non confondersi */
+          answers={answers}
+          voteUp={voteUp}
+          addAnswer={addAnswer}
+          /*chiamata differente per farci notare
+          che eventualmente possiamo chiamarlo in due maniere differenti */
+          editAnswer={updateAnswer}
+        />
       </Container>
     </>
-  )
-
+  );
 }
 
 export default App;
--- week07/react-qa/src/components/Answers.jsx
+++ week07/react-qa/src/components/Answers.jsx
@@ -3,36 +3,81 @@ import { Row, Col, Table, Button } from "react-bootstrap";
 import AnswerForm from "./AnswerForm";
 import { useState } from "react";
 
-function Answers (props) {
+function Answers(props) {
+  /*gestire il pulsante per la visione del form (stato ternario)
+  - view
+  - add
+  - edit*/
   const [mode, setMode] = useState("view");
+
+  /*noi vogliamo lo stato che ci mantenga la risposta da modificare
+  questo perchè la tabella e il form non sono uno dentro l'altro
+  => non possiamo passare le props perchè non è un figlio
+  
+  ci serve lo stato in answers perchè è il padre comune di questi elementi
+  (form e tabella)*/
   const [editableAnswer, setEditableAnswer] = useState();
 
   const handleEdit = (answer) => {
     setEditableAnswer(answer);
     setMode("edit");
-  }
+  };
 
-  return(
+  return (
     <>
-    <Row>
-      <Col as="h2">Answers:</Col>
-    </Row>
-    <Row>
-      <Col lg={10} className="mx-auto">
-        <AnswerTable answers={props.answers} voteUp={props.voteUp} handleEdit={handleEdit} />
+      <Row>
+        <Col as="h2">Answers:</Col>
+      </Row>
+      <Row>
+        <Col lg={10} className="mx-auto">
+          <AnswerTable
+            answers={props.answers}
+            voteUp={props.voteUp}
+            handleEdit={handleEdit}
+          />
 
-        { mode === "view" && <Button variant="primary" onClick={() => setMode("add")}>Add</Button>}
+          {mode === "view" && (
+            <Button variant="primary" onClick={() => setMode("add")}>
+              Add
+            </Button>
+          )}
 
-        { mode === "add" && <AnswerForm addAnswer={(answer) => {props.addAnswer(answer); setMode("view");}} cancel={() => setMode("view")}/>}
+          {mode === "add" && (
+            <AnswerForm
+              addAnswer={(answer) => {
+                /*aggiunge la risposta alla tabella */
+                props.addAnswer(answer);
+                /*cambia il tipo di visualizzazione */
+                setMode("view");
+              }}
+              /*cancel è una prop -> devo andarla ad inserire dove viene chiamata */
+              cancel={() => setMode("view")}
+            />
+          )}
 
-        { mode === "edit" && <AnswerForm key={editableAnswer.id} answer={editableAnswer} editAnswer={(answer) => {props.editAnswer(answer); setMode("view");}} cancel={() => setMode("view")} />}
-      </Col>
-    </Row>
+          {mode === "edit" && (
+            <AnswerForm
+              /*serve a react per capire che domanda andare ad inserire
+              => nel momento in cui cambia l'id della domanda lo rendirizza
+              -> caso in cui clicchiamo due volte consecutivamente due bottoni 
+              per modifica consecutivamente ma senza cliccare update 
+              o cancel per il form  */
+              key={editableAnswer.id}
+              answer={editableAnswer}
+              editAnswer={(answer) => {
+                props.editAnswer(answer);
+                setMode("view");
+              }}
+              cancel={() => setMode("view")}
+            />
+          )}
+        </Col>
+      </Row>
     </>
   );
 }
 
-function AnswerTable (props) {
+function AnswerTable(props) {
   return (
     <Table striped>
       <thead>
@@ -45,20 +90,38 @@ function AnswerTable (props) {
         </tr>
       </thead>
       <tbody>
-        { props.answers.map((ans) => <AnswerRow key={ans.id} answer={ans} voteUp={props.voteUp} handleEdit={props.handleEdit} />) }
+        {props.answers.map((ans) => (
+          <AnswerRow
+            /*con tabelle, liste, bisogna inserire delle key 
+            univoche per l'utilizzo di React*/
+            key={ans.id}
+            answer={ans}
+            voteUp={props.voteUp}
+            handleEdit={props.handleEdit}
+          />
+        ))}
       </tbody>
     </Table>
   );
 }
 
 function AnswerRow(props) {
-  return(
-    <tr><AnswerData answer={props.answer} /><AnswerAction {...props} /></tr>
+  return (
+    <tr>
+      <AnswerData answer={props.answer} />
+      <AnswerAction
+        /*avremmo potuto mettere 
+        {..props} per passarle tutte senza doverle elencare */
+        voteUp={props.voteUp}
+        answer={props.answer}
+        handleEdit={props.handleEdit}
+      />
+    </tr>
   );
 }
 
 function AnswerData(props) {
-  return(
+  return (
     <>
       <td>{props.answer.date.format("YYYY-MM-DD")}</td>
       <td>{props.answer.text}</td>
@@ -69,11 +132,26 @@ function AnswerData(props) {
 }
 
 function AnswerAction(props) {
-  return(
+  return (
     <td>
-      <Button variant="warning" onClick={() => {props.voteUp(props.answer.id)}}><i className="bi bi-arrow-up" /></Button>
-      <Button variant="primary" className="mx-1" onClick={() => props.handleEdit(props.answer)}><i className="bi bi-pencil-square" /></Button> 
-      <Button variant="danger"><i className="bi bi-trash" /></Button>
+      <Button
+        variant="warning"
+        onClick={() => {
+          props.voteUp(props.answer.id);
+        }}
+      >
+        <i className="bi bi-arrow-up" />
+      </Button>
+      <Button
+        variant="primary"
+        className="mx-1"
+        onClick={() => props.handleEdit(props.answer)}
+      >
+        <i className="bi bi-pencil-square" />
+      </Button>
+      <Button variant="danger">
+        <i className="bi bi-trash" />
+      </Button>
     </td>
   );
 }
